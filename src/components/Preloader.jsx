42e9cfece15ec290362cd89f1a4a412695fad9cc
Kush Tiwari <tiwarikush9328@gmail.com>
2025-12-04 00:43:03 +0530

    1.The spiral in the intro, made that bigger

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,11 +1,11 @@
 const Preloader = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
-      <div className="w-24 h-24 sm:w-28 sm:h-28 flex items-center justify-center animate-spin-slow">
+      <div className="w-32 h-32 sm:w-40 sm:h-40 flex items-center justify-center animate-spin-slow">
         <img
           src="/logo.ico"
           alt="Company Logo"
-          className="w-20 h-20 sm:w-24 sm:h-24 object-contain"
+          className="w-28 h-28 sm:w-32 sm:h-32 object-contain"
         />
       </div>
     </div>
